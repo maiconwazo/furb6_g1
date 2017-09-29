a3d2c62f93a324d1248be481d61c10a4ab186477
Maicon Santos da Silva <maiconsantos2501@gmail.com>
2017-09-28 22:01:04 -0300

    finalizado (falta dar display nos resultados)

--- IA/trabalho_algoritmo_genetico/CaixeiroViajante.m
+++ IA/trabalho_algoritmo_genetico/CaixeiroViajante.m
@@ -8,17 +8,11 @@ Nt=npar; % numero de colunas da matriz de populacao
 x=rand(1,npar);
 y=rand(1,npar); % cidades em (cidadeX, cidadeY)
 
-% Adiciona o numero maximo de interacoes (Criterio de Parada)
-
-% Parametros do AG
-% tamanho da populacao
-% taxa de mutacao (probabilidade de 0.05)
 tamPop = 20;
 selecao=0.5; % fracao da populacao a ser mantida
 manter=floor(selecao*tamPop); % numero de membros da populacao que sobrevivem
 M=ceil((tamPop-manter)/2); % numero de cruzamentos
 
-% calculo da probabilidade para a selecao dos pais
 probab=1; %probabilidade
 for ii=2:manter
     probab=[probab ii*ones(1,ii)];
@@ -26,11 +20,7 @@ end
 Nprobab=length(probab);
 probab=manter-probab+1; 
 
-% Gerar a populacao inicial
 iag=0; % contador para iniciar a geracao
-% gerar a populacao aleatoria (Veja funcao randperm)
-% deve ser uma matriz 20x20 (cromossomo + n. da populacao)
-
 for i = 1:tamPop
   pais(i,:) = randperm(npar);
   
@@ -41,13 +31,7 @@ endfor
 
 
 % calcular o custo da populacao utilizando a funcao de aptidao
-% colocar o custo minimo no elemento 1 (Veja funcao sort)
-% organizar a populacao com o custo mais baixo primeiro
-% calcula o custo minimo da populacao (veja funcao min)
-% calcula a media aritmetica da populacao (veja funcao mean)
-
-maxit=100;
-%% Interacao pelas geracoes (LOOP PRINCIPAL)
+maxit=10000;
 while iag<maxit
     for i = 1:tamPop
         populacao(i,:).aptidao = cvfun(pais(populacao(i,:).codigo,:));
@@ -64,7 +48,6 @@ while iag<maxit
     indPai1=probab(escolha1); % selecionar os indices escolhidos na roleta para o pai 1
     indPai2=probab(escolha2); % selecionar os indices escolhidos na roleta para o pai 2
     
-    % Execucao da Recombinacao (crossover)
     novaPos = 11;
     for ic=1:M        
         pai1 = pais(populacao(indPai1(ic),:).codigo,:);
@@ -82,26 +65,20 @@ while iag<maxit
             repetido = verificaGenteReptido(pais(populacao(novaPos).codigo,:), repetido);          
         end
         
+        mutacao1 = randi(20);
+        mutacao2 = randi(20);
+        
+        temp_mut = pais(populacao(novaPos).codigo,mutacao1);
+        pais(populacao(novaPos).codigo,mutacao1) = pais(populacao(novaPos).codigo,mutacao2);
+        pais(populacao(novaPos).codigo,mutacao2) = temp_mut;
+        
+        temp_mut = pais(populacao(novaPos + 1).codigo,mutacao1);
+        pais(populacao(novaPos + 1).codigo,mutacao1) = pais(populacao(novaPos + 1).codigo,mutacao2);
+        pais(populacao(novaPos + 1).codigo,mutacao2) =temp_mut;
+        
         novaPos = novaPos + 2;
     end    
-        %seleciona o Pai 1
-        %seleciona o Pai 2
-       
-        
-        % Faz a t�cnica de recombina��o Cycle
-
-    % Faz a Mutacao da populacao
-
-   
-    % Se calcula um novo custo para a nova popula��o
-    
-    
-    %_______________________________________________________
-    % Organiza em ordem crescente os custos e associa aos parametros
 end
     
 disp(pais);
-
-%_______________________________________________________
-% Mostrar os resultados
 
